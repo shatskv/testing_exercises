27889af7efd8dcbfbc36ebded4906c75df5a558c
Vladimir Shatskiy <vovaysach@gmail.com>
2023-12-07 18:59:40 +0300

    level_1 done

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,5 +1,36 @@
-from functions.level_1.four_bank_parser import BankCard, SmsMessage, Expense, parse_ineco_expense
+import datetime
 
+from functions.level_1.four_bank_parser import (BankCard, Expense, SmsMessage,
+                                                parse_ineco_expense)
+import pytest
 
 def test_parse_ineco_expense():
-    pass
+    card_one = BankCard('4567', 'Petrov')
+    card_two = BankCard('4511', 'Petrov')
+    default_text = '1096 45, 1234567812234511 06.12.23 13:12 4lapi authcode 345562'
+    default_time = datetime.datetime.now() - datetime.timedelta(hours=1)
+    company = 'Tinkoff'
+    message = SmsMessage(default_text, company, default_time)
+
+    expence = parse_ineco_expense(message, [card_one, card_two])
+
+    assert expence.amount == 1096
+    assert expence.card.last_digits == '4511'
+    assert expence.card.owner == 'Petrov'
+    assert expence.spent_in == '4lapi'
+    assert expence.spent_at == datetime.datetime(2023, 12, 6, 13, 12)
+    with pytest.raises(AttributeError):
+        message.text = '1096 45, 1234567812234511 06.12.23 13.12 4lapi authcode 345562'
+    with pytest.raises(ValueError):
+        message = SmsMessage('1096 45, 1234567812234511 06.12.23 13.12 4lapi authcode 345562',
+                             company, default_time)
+        parse_ineco_expense(message, [card_one, card_two])
+    with pytest.raises(ValueError):
+        message = SmsMessage('1096 45, 122323 4lapi authcode 345562',
+                             company, default_time)
+        parse_ineco_expense(message, [card_one, card_two])
+    with pytest.raises(IndexError):
+        message = SmsMessage('1096, 1234567812234511 06.12.23 13:12 4lapi authcode 345562',
+                             company, default_time)
+        parse_ineco_expense(message, [card_one, card_two])
+    
--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,5 +1,16 @@
+import pytest
+
 from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url('google.com', 'account', {'id': '56'}) == 'google.com/account?id=56'
+    assert build_url('google.com', 'account', {'id': 56}) == 'google.com/account?id=56'
+    assert build_url('google.com', 'account', {'id': '56', 'user': 'desar'}) == \
+                                        'google.com/account?id=56&user=desar'
+    assert build_url('google.com', 'account', None) == 'google.com/account'
+    
+
+def test_build_url_exceptions():
+    with pytest.raises(AttributeError):
+        build_url('google.com', 'account', 12)
